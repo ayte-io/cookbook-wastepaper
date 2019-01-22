5ba94a49b9104eadf27d8649956c8f2d1eeca7ec
Etki <etki@ayte.team>
2019-01-22 03:49:56 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Wastepaper Cookbook
+
+This cookbook exists to mimic real cookbook and test integrations with
+Chef, Supermarket, and so on.
+
---
+++ metadata.rb
@@ -0,0 +1,20 @@
+name 'ayte-wastepaper'
+maintainer 'Ayte Labs'
+maintainer_email 'ops@ayte.team'
+license 'MIT'
+description 'Cookbook for Chef/Supermarket integration testing'
+long_description 'Cookbook for Chef/Supermarket integration testing'
+version IO.read(File::join(__dir__, 'VERSION')).strip
+chef_version '>= 13.0'
+
+# The `issues_url` points to the location where issues for this cookbook are
+# tracked.  A `View Issues` link will be displayed on this cookbook's page when
+# uploaded to a Supermarket.
+#
+# issues_url 'https://github.com/<insert_org_here>/ayte-wastepaper/issues'
+
+# The `source_url` points to the development repository for this cookbook.  A
+# `View Source` link will be displayed on this cookbook's page when uploaded to
+# a Supermarket.
+#
+# source_url 'https://github.com/<insert_org_here>/ayte-wastepaper'
